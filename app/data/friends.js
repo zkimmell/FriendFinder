1380fc8efe7e206708f0bef922eac8da8e737884
zkimmell <zkimmell@yahoo.com>
2017-08-07 14:44:34 -0500

    Update friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+
+// Friends data
+
+
+
+var friends = [
+  {
+    name: "John",
+    photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwMDE4MzgzMF5BMl5BanBnXkFtZTYwMDQzMzU3._V1_UY317_CR48,0,214,317_AL_.jpg",
+    scores: [
+      "3",
+      "1",
+      "2",
+      "4",
+      "5",
+      "4",
+      "2",
+      "2",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Dan",
+    photo: "https://s-media-cache-ak0.pinimg.com/736x/01/b9/a1/01b9a1e80e242d4bc8d6a2e24da26a8d--dan-marino-nfl-football.jpg",
+    scores: [
+      "3",
+      "2",
+      "3",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Luke",
+    photo: "https://vignette1.wikia.nocookie.net/starwars/images/d/d9/Luke-rotjpromo.jpg/revision/latest/scale-to-width-down/350?cb=20091030151422",
+    scores: [
+      "2",
+      "2",
+      "5",
+      "2",
+      "4",
+      "3",
+      "3",
+      "2",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Tom",
+    photo: "http://sports.cbsimg.net/images/blogs/Tom-Brady-ten-years-10-21-15.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Chris",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Chris_Pratt_by_Gage_Skidmore_2.jpg/1200px-Chris_Pratt_by_Gage_Skidmore_2.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Tim",
+    photo: "http://gazettereview.com/wp-content/uploads/2016/12/tim-duncan.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
